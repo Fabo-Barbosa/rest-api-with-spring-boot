17f3d33a232b0305a0a813a7882ae7466bfef4f8
Fábio Barbosa <92115726+Fabo-Barbosa@users.noreply.github.com>
2025-07-25 21:22:26 -0300

    implementando o design patterns VO - Value Object

---
+++ 04_PersonValueObjetct/PersonModelAPI/src/main/java/br/com/fabodev/services/PersonServices.java
@@ -0,0 +1,69 @@
+package br.com.fabodev.services;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.fabodev.data.vo.v1.PersonVO;
+import br.com.fabodev.exceptions.ResourceNotFoundException;
+import br.com.fabodev.mapper.DozerMapper;
+import br.com.fabodev.model.Person;
+import br.com.fabodev.repositories.PersonRepository;
+
+@Service
+public class PersonServices {
+	
+	private final Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	@Autowired
+	PersonRepository repository;
+	
+	public List<PersonVO> findAll() {
+		logger.info("Finding all people!");
+		
+		return DozerMapper.parseListObject(repository.findAll(), PersonVO.class);
+	}
+	
+	public PersonVO getById(Long id) {
+		
+		logger.info("Finding new person by id!");		
+		
+		return DozerMapper.parseObject(repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!")), 
+				PersonVO.class);
+	}
+	
+	public PersonVO create(PersonVO person) {
+		logger.info("Creating person...");
+		
+		var entity = DozerMapper.parseObject(person, Person.class);
+		var vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+		return vo;
+	}
+	
+	public PersonVO update(PersonVO person) {
+		logger.info("Updating person...");
+		
+		var entity = repository.findById(person.getId())
+			.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		var vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+		return vo;
+	}
+	
+	public void delete(Long id) {
+		logger.info("Updating person...");
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		repository.delete(entity);
+	}
+
+}
+
---
+++ 04_PersonValueObjetct/PersonModelAPI/src/test/java/br/com/fabodev/RestApiComSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiComSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
