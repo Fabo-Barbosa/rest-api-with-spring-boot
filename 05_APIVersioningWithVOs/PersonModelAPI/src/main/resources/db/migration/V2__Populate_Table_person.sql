d6ee9323ebb6638bccd907715b46b70523974dd5
Fábio Barbosa <92115726+Fabo-Barbosa@users.noreply.github.com>
2025-08-04 10:43:20 -0300

    Implementing migration with flyway

---
+++ 05_APIVersioningWithVOs/PersonModelAPI/src/main/resources/db/migration/V2__Populate_Table_person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` VALUES (2,'Rua Castelo Branco','Rafael','Male','Barbosa'),
+					(3,'Rua Preto Amarelo','Joane','Female','Brenda'),
+					(4,'Rua Chico Mendes','Joana','Female','Drak'),
+					(5,'Rua Chico Mendes','Joana','Female','Drak'),
+					(6,'Rua Chico Mendes','Joana','Femle','Dak'),
+					(7,'Rua Chico Mendes','JJ','Femle','Dak'),
+					(8,'Jailson Chico Mendes','Zoan','Male','Dak'),
+					(9,'Jailson Chico Mendes','Zoan','Male','Dak');
